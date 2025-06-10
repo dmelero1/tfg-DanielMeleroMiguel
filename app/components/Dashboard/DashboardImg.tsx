5f0d08fd09d15d23c7cac3a71a89daf9a12a75b9
dmelero1 <danielmm1103@gmail.com>
2025-06-10 17:21:45 +0200

    Dashboard 3D video

--- app/components/Dashboard/DashboardImg.tsx
+++ app/components/Dashboard/DashboardImg.tsx
@@ -1,10 +1,17 @@
-import imgLogoCristal from "../Dashboard/ImgDashboard/imagenLogoCromatic.png";
+import React, { useRef, useEffect } from "react";
+import videoLogo from "./ImgDashboard/logoTFG.mp4";
 
 const DashboardImg = () => {
   return (
-    <div className="w-full bg-black text-white flex flex-col items-center justify-center text-center py-10 sm:py-16 px-4">
-      <h1 className="text-2xl sm:text-4xl font-bold">Bienvenido a DMSPORT</h1>
-      <img src={imgLogoCristal} alt="Logo" className="w-32 sm:w-56 h-auto mt-4 sm:mt-6" />
+    <div className="w-full bg-black text-white flex flex-col items-center justify-center text-center py-10 sm:py-0 px-4">      
+      <video
+        src={videoLogo}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-96 sm:w-8/12 h-auto rounded-xl shadow-lg"
+      />
     </div>
   );
 };
